7d79c9d600c0316097a8815afed6d5b7c1cb833d
Ccalos7319 <ccarlosvalencia73193563@gmail.com>
2023-09-15 11:05:05 -0500

    agregando funcionalidad api

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function index()
+    {
+        $usuarios = Usuario::all();
+        return $usuarios;
+    }
+
+    public function show($id)
+    {
+        $usuario = Usuario::find($id);
+
+        return $usuario;
+    }
+}
